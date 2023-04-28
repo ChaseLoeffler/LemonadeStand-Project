f65fc7d476e4ae497c78e8663434e0e6167ef875
ChaseLoeffler <chaselo609@gmail.com>
2023-04-27 20:40:35 -0400

    Added new member variables. Added new Method PredictWeather to Weather class.

--- LemonadeStand/Weather.cs
+++ LemonadeStand/Weather.cs
@@ -12,12 +12,26 @@ namespace LemonadeStand
         public string condition;
         public int temperature;
         private List<string> weatherConditions;
+        private List<string> forcast;
         public string predictedForecast;
+        Random rand;
         //Constructor
         public Weather()
         {
-            
+            weatherConditions = new List<string> {"Sunny","Cloudy","Rainy"};
+            forcast = new List<string> {"Hot and Sunny.","Warm and Cloudy","Cold and Rainy"};
+            rand = new Random();
+            condition = "";
+            temperature = 0;
+            predictedForecast = "";
         }
         //Member Methods (CAN DO)
+
+        public void PredictWeather()
+        {
+            predictedForecast = forcast[rand.Next(3)];
+            Console.WriteLine($"The weather looks like its going to be{predictedForecast}");
+        }
+
     }
 }
