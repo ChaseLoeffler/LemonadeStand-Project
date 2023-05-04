11e23206f743d2f753ac7fd6b24a89b30ba95a50
ChaseLoeffler <chaselo609@gmail.com>
2023-05-04 13:26:50 -0400

    Added member variables: Ramdom rand, Wallet wallet, and int priceOfObject to customer class and assigned vaules in the Constructor. Then created Methods WantsLemonade and BuysLemonade.

--- LemonadeStand/Customer.cs
+++ LemonadeStand/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +9,34 @@ namespace LemonadeStand
 {
     internal class Customer
     {   // Member Variables (HAS A)
-
+        public Random rand;
+        public Wallet wallet;
+        public int priceOfObject;
         //Constructor
         public Customer()
         {
-            
+            wallet = new Wallet();
+            rand = new Random();
+            priceOfObject = 0;
         }
         //Member Methods (CAN DO)
+        public bool WantsLemonade()
+        {
+            int randomNum = (int)rand.NextInt64(10);
+            if (randomNum <= 5)
+            {
+               return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void BuysLemonade()
+        {
+            this.wallet.PayMoneyForItems(priceOfObject);
+        }
+
+
     }
 }
