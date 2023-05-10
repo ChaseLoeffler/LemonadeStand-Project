cb43cf7b70cba5852cc1832351dc77ae5aa29520
ChaseLoeffler <chaselo609@gmail.com>
2023-05-10 17:15:48 -0400

    Created method CustomersWalkingBy to check the days weather and price of the lemonade to determine if the customers walking by perchase lemonade.

--- LemonadeStand/Day.cs
+++ LemonadeStand/Day.cs
@@ -10,11 +10,13 @@ namespace LemonadeStand
     {   // Member Variables (HAS A)
         public Weather weather;
         public List<Customer> customers;
+        public Random rand;
         //Constructor
         public Day()
         {
             weather = new Weather();
             customers = new List<Customer>();
+            rand = new Random();
         }
         //Member Methods (CAN DO)
         public void DaysPossibleWeather()
@@ -35,6 +37,60 @@ namespace LemonadeStand
             {
                 Customer customer = new Customer();
                 customers.Add(customer);
+            }
+        }
+
+        public void CustomersWalkingBy(double priceOfObject,int numberOfCups)
+        {
+            foreach (Customer customer in customers)
+            {
+                do
+                {
+                    Console.WriteLine("A customer is walking by.");
+                    bool wants = customer.WantsLemonade(weather.condition);
+                    if (wants == true)
+                    {
+                        int maxWillingToPay = 0;
+                        if (weather.condition == "Hot and Sunny")
+                        {
+                            maxWillingToPay = rand.Next(5, 11);
+                            bool okayWithPaying = customer.ChecksPrice(priceOfObject, maxWillingToPay);
+                            if (okayWithPaying == true)
+                            {
+                                Console.WriteLine("A customer perchased Lemonade.\n");
+                                customer.BuysLemonade(priceOfObject);
+                            }
+                        }
+                        if (weather.condition == "Warm and Cloudy")
+                        {
+                            maxWillingToPay = rand.Next(3, 6);
+                            bool okayWithPaying = customer.ChecksPrice(priceOfObject, maxWillingToPay);
+                            if (okayWithPaying == true)
+                            {
+                                Console.WriteLine("A customer perchased Lemonade.\n");
+                                customer.BuysLemonade(priceOfObject);
+                            }
+                        }
+                        if (weather.condition == "Cold and Rainy")
+                        {
+                            maxWillingToPay = rand.Next(0,2);
+                            bool okayWithPaying = customer.ChecksPrice(priceOfObject, maxWillingToPay);
+                            if (okayWithPaying == true)
+                            {
+                                Console.WriteLine("A customer perchased Lemonade.\n");
+                                customer.BuysLemonade(priceOfObject);
+                            }
+                        }
+
+                    }
+                    --numberOfCups;
+                } while (numberOfCups > 0);
+                if (numberOfCups == 0)
+                {
+                    Console.WriteLine("You have sold out of Lemonade. You pack up your things for the day and leave.");
+                    break;
+                }
+
             }
         }
     }
